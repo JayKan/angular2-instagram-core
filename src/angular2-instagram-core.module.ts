5f3e8a707dbfb489a752162ecb978824a7ccac5d
Jay Kan <leicasper@gmail.com>
2017-04-24 13:59:08 -0700

    chore: add forChild hook

--- src/angular2-instagram-core.module.ts
+++ src/angular2-instagram-core.module.ts
@@ -1,19 +1,24 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
+import { FiltersActions } from './actions';
+import { FiltersService } from './services';
 
+export * from './effects';
+export * from './reducers';
+export * from './interfaces';
 
 @NgModule({})
 export class Angular2InstagramCoreModule {
   /**
    * @description Use this method in your other (non root) modules to import the services
-   * @param config
    * @returns {ModuleWithProviders}
    */
-  static forChild(config): ModuleWithProviders {
+  static forChild(): ModuleWithProviders {
     return {
       ngModule: Angular2InstagramCoreModule,
       providers: [
-
+        FiltersActions,
+        FiltersService
       ]
-    }
+    };
   }
 }
