1fbb2841a18d6986a48b9c3e3770ce4a16691c88
Jay Kan <leicasper@gmail.com>
2017-04-25 13:57:35 -0700

    refactor(selectors): clean up

--- src/reducers/selectors.ts
+++ src/reducers/selectors.ts
@@ -3,102 +3,104 @@ import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/let';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
 import { AppState, FiltersState, FilterStyle, OverlayStyle } from '../interfaces';
 
-export function getFiltersState(state$: Observable<AppState>): Observable<FiltersState> {
-  return state$.select((state: AppState) => state.filters);
+export function getFiltersState(store$: Store<AppState>): Observable<FiltersState> {
+  return store$.select((state: AppState) => state.filters)
+    .distinctUntilChanged();
 }
 
-export function getFilterStyleValue(state$: Observable<AppState>): Observable<FilterStyle> {
-  return state$.let(getFiltersState)
+export function getFilterStyleValue(store$: Store<AppState>): Observable<FilterStyle> {
+  return store$.let(getFiltersState)
     .map((filtersState: FiltersState) => filtersState.get('styles').toJS())
     .distinctUntilChanged((previous: FilterStyle, next: FilterStyle) => {
       return previous.WebkitFilter === next.WebkitFilter && previous.filter === next.filter;
     });
 }
 
-export function getOverlayStyleValue(state$: Observable<AppState>): Observable<OverlayStyle> {
-  return state$.let(getFiltersState)
+export function getOverlayStyleValue(store$: Store<AppState>): Observable<OverlayStyle> {
+  return store$.let(getFiltersState)
     .map((filtersState: FiltersState) => filtersState.get('overlay').toJS())
     .distinctUntilChanged((previous: OverlayStyle, next: OverlayStyle) => {
       return previous.background === next.background;
     });
 }
 
-export function getContrastFilterValue(state$: Observable<AppState>): Observable<number> {
-  return state$.let(getFiltersState)
+export function getContrastFilterValue(store$: Store<AppState>): Observable<number> {
+  return store$.let(getFiltersState)
     .map((filtersState: FiltersState) => filtersState.get('contrast'))
     .distinctUntilChanged();
 }
 
-export function getBrightnessFilterValue(state$: Observable<AppState>): Observable<number> {
-  return state$.let(getFiltersState)
+export function getBrightnessFilterValue(store$: Store<AppState>): Observable<number> {
+  return store$.let(getFiltersState)
     .map((filtersState: FiltersState) => filtersState.get('brightness'))
     .distinctUntilChanged();
 }
 
-export function getSaturateFilterValue(state$: Observable<AppState>): Observable<number> {
-  return state$.let(getFiltersState)
+export function getSaturateFilterValue(store$: Store<AppState>): Observable<number> {
+  return store$.let(getFiltersState)
     .map((filtersState: FiltersState) => filtersState.get('saturate'))
     .distinctUntilChanged();
 }
 
-export function getSepiaFilterValue(state$: Observable<AppState>): Observable<number> {
-  return state$.let(getFiltersState)
+export function getSepiaFilterValue(store$: Store<AppState>): Observable<number> {
+  return store$.let(getFiltersState)
     .map((filtersState: FiltersState) => filtersState.get('sepia'))
     .distinctUntilChanged();
 }
 
-export function getGrayScaleFilterValue(state$: Observable<AppState>): Observable<number> {
-  return state$.let(getFiltersState)
+export function getGrayScaleFilterValue(store$: Store<AppState>): Observable<number> {
+  return store$.let(getFiltersState)
     .map((filtersState: FiltersState) => filtersState.get('grayScale'))
     .distinctUntilChanged();
 }
 
-export function getInvertFilterValue(state$: Observable<AppState>): Observable<number> {
-  return state$.let(getFiltersState)
+export function getInvertFilterValue(store$: Store<AppState>): Observable<number> {
+  return store$.let(getFiltersState)
     .map((filersState: FiltersState) => filersState.get('invert'))
     .distinctUntilChanged();
 }
 
-export function getHueRotateFilterValue(state$: Observable<AppState>): Observable<number> {
-  return state$.let(getFiltersState)
+export function getHueRotateFilterValue(store$: Store<AppState>): Observable<number> {
+  return store$.let(getFiltersState)
     .map((filtersState: FiltersState) => filtersState.get('hueRotate'))
     .distinctUntilChanged();
 }
 
-export function getBlurFilterValue(state$: Observable<AppState>): Observable<number> {
-  return state$.let(getFiltersState)
+export function getBlurFilterValue(store$: Store<AppState>): Observable<number> {
+  return store$.let(getFiltersState)
     .map((filtersState: FiltersState) => filtersState.get('blur'))
     .distinctUntilChanged();
 }
 
-export function getBlendFilterValue(state$: Observable<AppState>): Observable<string> {
-  return state$.let(getFiltersState)
+export function getBlendFilterValue(store$: Store<AppState>): Observable<string> {
+  return store$.let(getFiltersState)
     .map((filtersState: FiltersState) => filtersState.get('blend'))
     .distinctUntilChanged();
 }
 
-export function getSelectedImage(state$: Observable<AppState>): Observable<string> {
-  return state$.let(getFiltersState)
+export function getSelectedImage(store$: Store<AppState>): Observable<string> {
+  return store$.let(getFiltersState)
     .map((filtersState: FiltersState) => filtersState.get('selectedImage'))
     .distinctUntilChanged();
 }
 
-export function getImages(state$: Observable<AppState>): Observable<any> {
-  return state$.let(getFiltersState)
+export function getImages(store$: Store<AppState>): Observable<any> {
+  return store$.let(getFiltersState)
     .map((filtersState: FiltersState) => filtersState.get('images').toJS())
     .distinctUntilChanged();
 }
 
-export function getError(state$: Observable<AppState>): Observable<string> {
-  return state$.let(getFiltersState)
+export function getError(store$: Store<AppState>): Observable<string> {
+  return store$.let(getFiltersState)
     .map((filtersState: FiltersState) => filtersState.get('error'))
     .distinctUntilChanged();
 }
 
-export function getLoading(state$: Observable<AppState>): Observable<boolean> {
-  return state$.let(getFiltersState)
+export function getLoading(store$: Store<AppState>): Observable<boolean> {
+  return store$.let(getFiltersState)
     .map((filtersState: FiltersState) => filtersState.get('loading'))
     .distinctUntilChanged();
 }
